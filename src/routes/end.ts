13a6f785d9ae64cb831e28f9ffc5f72afc17db07
Susie Coleman <login@susiecoleman.me>
2020-01-24 10:38:27 +0000

    feat: add link to the end page to direct users back to the start

--- src/routes/end.ts
+++ src/routes/end.ts
@@ -2,9 +2,11 @@ import { Request, Response, Application } from 'express';
 
 const buildEndRoute = (app: Application) => {
   app.get('/end', (req: Request, res: Response) => {
-    res.render('infoGeneric', {
+    res.render('infoButtonGeneric', {
       title: 'Evaluation Complete',
       subtitle: 'Thank you for taking part.',
+      url: '/',
+      buttonText: 'Evaluate another set',
     });
   });
 };
