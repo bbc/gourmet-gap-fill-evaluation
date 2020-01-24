22e06c60b5758de9b6516ad87a9b2b61ca6336c5
Susie Coleman <login@susiecoleman.me>
2020-01-24 10:18:20 +0000

    feat: When displaying segment sets to user order them alphabetically by name

--- src/routes/start.ts
+++ src/routes/start.ts
@@ -5,7 +5,10 @@ const buildStartRoute = (app: Application) => {
   app.get('/start', (req: Request, res: Response) => {
     getSegmentSets()
       .then(segmentSets => {
-        res.render('start', { segmentSets, evaluatorIds });
+        const setsOrderedByName = segmentSets.sort((a, b) =>
+          a.name.localeCompare(b.name)
+        );
+        res.render('start', { segmentSets: setsOrderedByName, evaluatorIds });
       })
       .catch(error => {
         console.error(`Could not get segment sets ${error}`);
