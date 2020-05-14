ea5acd404ef319595a88798912642f87f9f7d92a
aniablaziak <anblaziak@gmail.com>
2020-05-14 15:12:29 +0100

    feat: remove duplicate answers with the same evaluatorId

--- src/routes/__tests__/exportdata.test.ts
+++ src/routes/__tests__/exportdata.test.ts
@@ -1,8 +1,11 @@
-import { convertSegmentAnswerToGapAnswer } from '../exportData';
+import {
+  convertSegmentAnswerToGapAnswer,
+  removeDuplicateAnswers,
+} from '../exportData';
 import { SegmentAnswer, GapAnswer } from '../../models/models';
 
 describe('convertSegmentAnswerToGapAnswer', () => {
-  test('', () => {
+  test('should convert segment answer to gap fill answers', () => {
     const segmentAnswer = new SegmentAnswer(
       'segmentId',
       'evaluatorId1',
@@ -58,5 +61,88 @@ describe('convertSegmentAnswerToGapAnswer', () => {
     expect(convertSegmentAnswerToGapAnswer(segmentAnswer)).toEqual(
       expectedResult
     );
+  });
+});
+
+describe('removeDuplicateAnswers', () => {
+  test('should remove duplicate old answers from segments', () => {
+    const segmentId1AnswerDuplicate = new SegmentAnswer(
+      'segmentId1',
+      'evaluatorId1',
+      ['a', 'b'],
+      12,
+      'sourceLanguage',
+      'translationSystem',
+      ['a', 'b'],
+      'hint',
+      'problem',
+      'source',
+      'translation',
+      'answerId',
+      200
+    );
+
+    const segmentId1AnswerEarliest = new SegmentAnswer(
+      'segmentId1',
+      'evaluatorId1',
+      ['c', 'd'],
+      12,
+      'sourceLanguage',
+      'translationSystem',
+      ['a', 'b'],
+      'hint',
+      'problem',
+      'source',
+      'translation',
+      'answerId',
+      100
+    );
+
+    const segmentId1Answer = new SegmentAnswer(
+      'segmentId1',
+      'evaluatorId2',
+      ['a', 'b'],
+      12,
+      'sourceLanguage',
+      'translationSystem',
+      ['a', 'b'],
+      'hint',
+      'problem',
+      'source',
+      'translation',
+      'answerId',
+      123
+    );
+
+    const segmentId2Answer = new SegmentAnswer(
+      'segmentId2',
+      'evaluatorId1',
+      ['a', 'b'],
+      12,
+      'sourceLanguage',
+      'translationSystem',
+      ['a', 'b'],
+      'hint',
+      'problem',
+      'source',
+      'translation',
+      'answerId',
+      123
+    );
+
+    const segmentAnswers = [
+      segmentId1AnswerDuplicate,
+      segmentId1AnswerEarliest,
+      segmentId1Answer,
+      segmentId2Answer,
+    ];
+
+    const expectedResult = [
+      segmentId1AnswerEarliest,
+      segmentId1Answer,
+      segmentId2Answer,
+    ];
+
+    expect(removeDuplicateAnswers(segmentAnswers)).toEqual(expectedResult);
   });
 });
