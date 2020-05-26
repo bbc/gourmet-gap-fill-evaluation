56ce270946f81897ad8a761a0bd663c643d43bbf
aniablaziak <anblaziak@gmail.com>
2020-05-26 17:19:10 +0100

    feat: display only unused evaluator ids

--- src/routes/start.ts
+++ src/routes/start.ts
@@ -15,15 +15,21 @@ const buildStartRoute = (app: Application) => {
           setsOrderedByName.find(set => set.name === setName) || null;
 
         let possibleEvaluatorIds: string[] = [];
+        let usedEvaluatorIds: string[] = [];
+        let unusedEvaluatorIds: string[] = [];
 
         if (selectedSet && selectedSet.possibleEvaluatorIds) {
           possibleEvaluatorIds = Array.from(selectedSet.possibleEvaluatorIds);
+          usedEvaluatorIds = Array.from(selectedSet.evaluatorIds || new Set());
+          unusedEvaluatorIds = possibleEvaluatorIds.filter(
+            item => !usedEvaluatorIds.includes(item)
+          );
         }
 
         res.render('start', {
           setName,
           segmentSets: setsOrderedByName,
-          possibleEvaluatorIds: [...possibleEvaluatorIds, 'tester'],
+          possibleEvaluatorIds: [...unusedEvaluatorIds, 'tester'],
           paragraphs: [
             'The evaluation process is timed but there is no time limit.',
             'Please complete a series evaluation in an unbroken single sitting so that the timing is reflective of the time spent on the task.',
